d2136f104b52ccd12eb5858c5319f993f33b7365
orchid0809 <ljllgrlove@163.com>
2018-10-28 20:12:04 +0800

    first commit

---
+++ src/main/java/com/example/demo/JenkinsbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsbootApplication.class, args);
+	}
+}
